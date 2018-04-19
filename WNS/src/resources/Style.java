9eb81244bc7941b992ce4554b4709bc6217ee9e8
ReyesEvan <carlos.reyesea@udlap.mx>
2018-04-18 23:25:36 -0500

    Or... if I do a commit of the whole project is enough?

---
+++ WNS/src/resources/Style.java
@@ -0,0 +1,39 @@
+/*
+ * Carlos Andrés Reyes Evangelista
+ * 157068
+ * Ingeniería en Sistemas Computacionales. UDLAP.
+ */
+package resources;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Reyes
+ */
+public class Style {
+    //    /*
+//    C O L O R   P A L E T T E :   C H A R L Y 
+//    */
+//    static Color base      = new Color (24, 41, 70);
+//    static Color primary   = new Color (14, 20, 32);
+//    static Color secondary = new Color (21, 31, 51);
+//    static Color tertiary  = new Color (136, 90, 27);
+////    static Color tertiary  = new Color (21, 44, 86);
+//    static Color light     = new Color (25, 58, 118);
+//    static Color highlight = new Color (156, 110, 27);
+//    public static Color foreground= new Color (220, 220, 220);
+//    int c           = 20;
+//    
+    /*
+        C O L O R   P A L E T T E :   D A R K
+    */
+    public static Color primary    = new Color (32, 36, 43); //background
+    public static Color secondary  = new Color (40, 45, 54); //second background
+    public static Color tertiary   = new Color (21, 44, 86); //right now, not used
+    public static Color light      = new Color (138, 140, 143); //lighten items
+    public static Color dark       = new Color (25, 28, 33); //items almost black
+    public static Color highlight  = new Color (156, 110, 27); //still not used
+    public static Color foreground = new Color (225, 225, 225); //every font and icons (not yet)
+    public int c           = 20;
+}
